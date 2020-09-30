a86556aebb0828e10eab9d8803b94f399527a65a
Andrey Sharaevskiy <aash7133@gmail.com>
2020-09-30 15:53:27 +0700

    Неделя 5 готов аккордеон Меню полностью

--- js/menuAccordeon.js
+++ js/menuAccordeon.js
@@ -1,5 +1,27 @@
-const mesureWidth = () => {
-	return 500;
+const mesureWidth = (item) => {
+	let reqItemWidth = 0;
+	const screenWidth = $(window).width();
+	const container = item.closest(".mainMenu");
+	const titlesBlocks = container.find(".mainMenu__title");
+	const titlesWidth = titlesBlocks.width() * titlesBlocks.length;
+
+	const textContainer = item.find(".mainMenu__container");
+	const paddingLeft = parseInt(textContainer.css("padding-left"));
+	const paddingRight = parseInt(textContainer.css("padding-right"));
+
+
+	const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
+	if (isMobile) {
+		reqItemWidth = screenWidth - titlesWidth;
+	} else {
+		reqItemWidth = 500;
+	}
+
+	return {
+		container: reqItemWidth,
+		textContainer: reqItemWidth - paddingRight - paddingLeft
+	}
 };
 
 const closeEveryItemInContainer = (container) => {
@@ -12,10 +34,12 @@ const closeEveryItemInContainer = (container) => {
 
 const openItem = (item) => {
 	const hiddenContent = item.find(".mainMenu__content");
-	const reqWidth = mesureWidth();
+	const reqWidth = mesureWidth(item);
+	const textBlock = item.find(".mainMenu__container");
 
 	item.addClass("active");
-	hiddenContent.width(reqWidth);
+	hiddenContent.width(reqWidth.container);
+	textBlock.width(reqWidth.textContainer)
 };
 
 $(".mainMenu__title").on("click", (e) => {
